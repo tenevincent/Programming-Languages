7321dd0b1064fddd5ce7d1153afcad5ae627be83
vincent tene <vincent.tene@gmail.com>
2021-01-28 13:14:53 +0100

    first commit

---
+++ CS_Nutshell/Chapter2/Chapter2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 12 * 30;              //     Statement 1
+            //Console.WriteLine(x);        //     Statement 2
+
+            Console.WriteLine(FeetToInches(30));      // 360
+            Console.WriteLine(FeetToInches(100));     // 1200
+
+            Point? p = null;    // This line will not compile.
+            int? x = null;    // Illegal, too.
+
+            Console.WriteLine(p.HasValue);
+            Console.WriteLine(x.HasValue);
+
+            Console.ReadKey();
+        }
+
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+
+    }
+
+
+    // A value type cannot ordinarily have a null value:
+
+    public struct Point { public int X, Y; }
+    
+    
+    public class Panda
+    {
+        public string Name;             // Instance field
+        public static int Population;   // Static field
+
+        public Panda(string n)         // Constructor
+        {
+            Name = n;                      // Assign the instance field
+            Population = Population + 1;   // Increment the static Population field
+        }
+    }
+
+}
---
+++ CS_Nutshell/Chapter2/Chapter2/Statements.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    class Statements
+    {
+        static void Main(string[] args)
+        {
+
+            TellMeTheType(12);
+            TellMeTheType("hello");
+            TellMeTheType(true);
+            var anyObject = new { Name = "Tene" };
+            TellMeTheType(anyObject);
+
+            object x = anyObject;
+            switch (x)
+            {
+                case bool b when b == true:     // Fires only when b is true
+                    Console.WriteLine("True!");
+                    break;
+                case bool b:
+                    Console.WriteLine("False!");
+                    break;
+                default:
+                    Console.WriteLine("The value is not a boolean");
+                    break;
+            }
+
+            object xdata = 3000F;
+
+            switch (xdata)
+            {
+                case float f when f > 1000:
+                    Console.WriteLine("The value is a float");
+                    break;
+                case double d when d > 1000:
+                    Console.WriteLine("The value is double");
+                    break;
+                case decimal m when m > 1000:
+                    Console.WriteLine("We can refer to x here but not f or d or m");
+                    break;
+            }
+
+
+
+        }
+
+
+
+        static void TellMeTheType(object x)   // object allows any type.
+        {
+            switch (x)
+            {
+                case int i:
+                    Console.WriteLine("It's an int!");
+                    Console.WriteLine($"The square of {i} is {i * i}");
+                    break;
+                case string s:
+                    Console.WriteLine("It's a string");
+                    Console.WriteLine($"The length of {s} is {s.Length}");
+                    break;
+                default:
+                    Console.WriteLine("I don't know what x is");
+                    break;
+            }
+        }
+
+
+
+    }
+}
